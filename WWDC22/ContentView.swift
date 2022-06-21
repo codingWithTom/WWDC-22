3fb71db76d4c25898cb8abf35572799442e978dc
Tomas Trujillo <t.trujillo138@gmail.com>
2022-06-20 17:43:28 -0700

    Initial Commit

---
+++ WWDC22/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WWDC22
+//
+//  Created by Tomas Trujillo on 2022-06-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
